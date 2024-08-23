6cfbe7e71238fd0ca35b590cbe765d1c098171bc
Nacho Garmilla <ngarmilla@gmail.com>
2024-08-23 21:27:19 +0200

    nosenidondeestoy

--- Examples/hiChannel - example.bat
+++ Examples/hiChannel - example.bat
@@ -1,2 +1,6 @@
+<<<<<<< HEAD
 "C:\Program Files\Python39\python.exe" "c:\Code\Python\HiChannel\hiChannel.py" "-timetable" "-confirm"
+=======
+"C:\Program Files\Python39\python.exe" "c:\Code\Python\HiChannel\hiChannel.py" "-timetable" "-confirm"
+>>>>>>> 71a031f59db4895d89e425016deef5df660bde5c
 exit
